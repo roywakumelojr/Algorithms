08b2754025a97c93042820a332ede5a61e5bc49a
Roy Wakumelo Jr <roywakumelo@roys-mbp.attlocal.net>
2020-02-20 15:51:53 -0500

    Rock paper scissors compete

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python
 
 import sys
-
+game_choices = [['rock'], ['paper'], ['scissors']]
 def rock_paper_scissors(n):
-  pass 
+  if n == 0:
+    return [[]]
+  if n == 1:
+    return game_choices
+  
+  result = []
 
+  first_play = rock_paper_scissors( n - 1 )
+
+  for new_game_choices in first_play:
+    for move in game_choices:
+      new_move = new_game_choices + move
+      result.append(new_move)
+  return result
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
